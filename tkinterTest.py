9a10190f9dbf2770e0d05e218c72b1e5c4568359
jonathanmcc-art <89641229+jonathanmcc-art@users.noreply.github.com>
2021-09-13 21:44:35 -0400

    commit test. python file with tkinter code

---
+++ tkinterTest.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root = Tk() # initializes window
+root.title("My Window Start")
+root.geometry("900x900")
+l = Label(root, text ="Kello World")
+l.pack()
+root.mainloop() #creates window loop
